09cc69f0277231349dad244ef385cafb39921f5c
inu93 <82743280+inu93@users.noreply.github.com>
2021-08-17 13:39:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Support 🇮🇩" ,url="https://t.me/katakatauntukmu") ],               [InlineKeyboardButton("Creator", url="https://t.me/ebnudoang") ]   ]  ) )
+	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("📍Instagram" ,url="https://www.instagram.com/ebnu_am/") ],               [InlineKeyboardButton("👤Creator", url="https://t.me/ebnudoang") ]   ]  ) )
                   
 
 
